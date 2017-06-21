ef416ca6ac81ad220bcaccb0ffcc40c78ca495f8
Cause Cheng <cuzfrog@139.com>
2017-06-21 18:04:22 +0800

    change ip and add apt mirrors

--- controller/controller-provision.sh
+++ controller/controller-provision.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
+
+# Install mariadb and create keystone database.
 apt install mariadb-server python-pymysql
 cp /vagrant/mariadb.conf /etc/mysql/mariadb.conf.d/99-openstack.cnf
 service mysql restart
+mysql -u root -e 'CREATE DATABASE keystone;'
+mysql -u root -e $'GRANT ALL PRIVILEGES ON keystone.* TO `keystone`@`%` IDENTIFIED BY \'boss1\';'
+mysql -u root -e $'GRANT ALL PRIVILEGES ON keystone.* TO `keystone`@`localhost` IDENTIFIED BY \'boss1\';'
+
+# Install RabbitMQ and memcached
 apt install rabbitmq-server
 rabbitmqctl add_user openstack boss1
 rabbitmqctl set_permissions openstack ".*" ".*" ".*"
 apt install memcached python-memcache
 service memcached restart
 
-mysql -u root -e 'CREATE DATABASE keystone;'
-mysql -u root -e $'GRANT ALL PRIVILEGES ON keystone.* TO `keystone`@`%` IDENTIFIED BY \'boss1\';'
-mysql -u root -e $'GRANT ALL PRIVILEGES ON keystone.* TO `keystone`@`localhost` IDENTIFIED BY \'boss1\';'
-
+# Install keystone and bootstrap
 apt install keystone
 cp /vagrant/keystone.conf /etc/keystone/keystone.conf
 su -s /bin/sh -c "keystone-manage db_sync" keystone
--- controller/provision.sh
+++ controller/provision.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 username=ubuntu
-#sudo useradd -s /bin/bash -d /opt/stack -m stack
 echo "${username} ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/${username}
 sudo su - ${username}
 
-
+# Basic provision
 cat /vagrant/hosts > /etc/hosts
 cat /vagrant/envs >> /etc/environment
 cat /vagrant/apt-proxy > /etc/apt/apt.conf
-cp /vagrant/apt-sources.list /etc/apt/sources.list
+cp /vagrant/artful-au-sources.list /etc/apt/sources.list
 
+# Initialize apt
+apt update
+apt install -y software-properties-common python-software-properties
+add-apt-repository -y cloud-archive:newton
+apt update
 
-#apt update
-#apt install -y software-properties-common python-software-properties
-#add-apt-repository -y cloud-archive:newton
-#apt update
-#apt install -y chrony
-#echo "allow 192.168.0.0/24" >> /etc/chrony/chrony.conf
-#service chrony restart
-#apt install -y python-openstackclient
+# Install OpenStack-cli and chrony
+apt install -y python-openstackclient
+apt install -y chrony
+echo "allow 172.22.6.0/24" >> /etc/chrony/chrony.conf
+service chrony restart
