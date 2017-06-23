1786e01457c6d927957c0fee3421da21ab2c9531
Cause Cheng <cuzfrog@139.com>
2017-06-23 17:29:10 +0800

    Fix cinder.conf

--- controller/controller-provision.sh
+++ controller/controller-provision.sh
@@ -92,14 +92,14 @@ openstack service create --name neutron --description "OpenStack Networking" net
 # Setup cinder (controller)
 openstack user create --domain default --password cinder_password cinder
 openstack role add --project service --user cinder admin
-#openstack service create --name cinder --description "OpenStack Block Storage" volume
-#openstack service create --name cinderv2 --description "OpenStack Block Storage" volumev2
-#openstack endpoint create --region RegionOne volume public http://controller:8776/v1/%\(tenant_id\)s
-#openstack endpoint create --region RegionOne volume internal http://controller:8776/v1/%\(tenant_id\)s
-#openstack endpoint create --region RegionOne volume admin http://controller:8776/v1/%\(tenant_id\)s
-#openstack endpoint create --region RegionOne volumev2 public http://controller:8776/v2/%\(tenant_id\)s
-#openstack endpoint create --region RegionOne volumev2 internal http://controller:8776/v2/%\(tenant_id\)s
-#openstack endpoint create --region RegionOne volumev2 admin http://controller:8776/v2/%\(tenant_id\)s
+openstack service create --name cinder --description "OpenStack Block Storage" volume
+openstack service create --name cinderv2 --description "OpenStack Block Storage" volumev2
+openstack endpoint create --region RegionOne volume public http://controller:8776/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volume internal http://controller:8776/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volume admin http://controller:8776/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 public http://controller:8776/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 internal http://controller:8776/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 admin http://controller:8776/v2/%\(tenant_id\)s
 openstack service create --name cinderv3 --description "OpenStack Block Storage" volumev3
 openstack endpoint create --region RegionOne volumev3 public http://controller:8776/v3/%\(tenant_id\)s
 openstack endpoint create --region RegionOne volumev3 internal http://controller:8776/v3/%\(tenant_id\)s
@@ -109,4 +109,4 @@ cp /vagrant/conf/cinder.conf /etc/cinder/cinder.conf
 su -s /bin/sh -c "cinder-manage db sync" cinder
 service nova-api restart
 service cinder-scheduler restart
-#service cinder-api restart
+service cinder-api restart
